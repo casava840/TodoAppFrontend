f141079b28d3e797a098e8254c0f9dd3bad657c0
casava840 <hen3403@gmail.com>
2024-01-28 18:49:45 +0900

    add insert function to TodoInsert.js

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []); //컴포넌트 리렌더링시 재사용을 통한 성능향상을 위해 onChange를 정의해줌
+
     return (
         <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요" />
+            <input
+            placeholder="할 일을 입력하세요"
+            value={value}
+            onChange={onChange} />
             <button type="submit">
                 <MdAdd />
             </button>
